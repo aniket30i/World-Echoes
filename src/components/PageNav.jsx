9e718b41c39cda027c700b6f6724d4761b6a3ef5
aniket30i <iamaniket.saha.09@protonmail.com>
2024-04-13 17:26:18 +0530

    ...

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/Pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/Product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Login">Login</NavLink>
         </li>
       </ul>
     </nav>
