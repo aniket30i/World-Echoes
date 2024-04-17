48b69a53433ea08edb2c9e8d59f9b12f25b72734
aniket30i <iamaniket.saha.09@protonmail.com>
2024-04-17 21:35:42 +0530

    Using several router hooks : useSearchParam , useNavigate and a small hiatus

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import { Navigate } from "react-router-dom";
 
 const BASEURL = "http://localhost:8000";
 
@@ -38,19 +41,17 @@ function App() {
         <Route path="Pricing" element={<Pricing />} />
         <Route path="Login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="Countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
       </Routes>
     </BrowserRouter>
